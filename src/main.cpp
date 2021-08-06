888589a2fde8edf909040be7115e41e48cc6e44f
Felix <photoninfinity@gmail.com>
2021-08-06 14:02:04 +0100

    Testing resistor ladder

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,20 @@
 #include <Arduino.h>
+#include <Servo.h>
+
+Servo lock;             // Locking mechanism
+const int button = A0;  // Analog read of voltage at end of resistor ladder
+int buttonVal;
 
 void setup() {
-  // put your setup code here, to run once:
+  lock.attach(3);
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  buttonVal = analogRead(A0);
+
+  if (buttonVal != 0) {
+    Serial.println(buttonVal);
+    delay(100);
+  }
 }
