0b9d25c5c7a56739556cf6d41d0e6ac3bc062799
Felix <photoninfinity@gmail.com>
2021-08-06 17:49:20 +0100

    optimised read buttons function

--- src/main.cpp
+++ src/main.cpp
@@ -25,43 +25,29 @@ int readButtons(int buttonsArr[4]) {  // Function to take button inputs and
     int buttonVal = analogRead(A0);  // Read voltage at end of resistor ladder
 
     if (buttonVal < 50) {  // Ignore noise
-
-    } else if (buttonVal >= 50 && buttonVal <= 180) {  // button 0 pressed
-      buttonsArr[buttonsPressed] = 0;  // Store 0 input in buttons array
-      buttonsPressed++;                // Increment button counter by 1
-      delay(200);                      // Delay to avoid multiple inputs
-      Serial.println(buttonsPressed);
-    } else if (buttonVal > 180 && buttonVal <= 220) {  // button 1 pressed
-      buttonsArr[buttonsPressed] = 1;  // Store 1 input in buttons array
-      buttonsPressed++;
-      delay(200);
-      Serial.println(buttonsPressed);
-    } else if (buttonVal > 220 && buttonVal <= 290) {  // button 2 pressed
-      buttonsArr[buttonsPressed] = 2;  // Store 2 input in buttons array
-      buttonsPressed++;
-      delay(200);
-      Serial.println(buttonsPressed);
-    } else if (buttonVal > 290 && buttonVal <= 400) {  // button 3 pressed
-      buttonsArr[buttonsPressed] = 3;  // Store 3 input in buttons array
-      buttonsPressed++;
-      delay(200);
-      Serial.println(buttonsPressed);
-    } else if (buttonVal > 400 && buttonVal <= 750) {  // button 4 pressed
-      buttonsArr[buttonsPressed] = 4;  // Store 4 input in buttons array
-      buttonsPressed++;
-      delay(200);
-      Serial.println(buttonsPressed);
-    } else if (buttonVal > 750 && buttonVal <= 1024) {  // button 5 pressed
-      buttonsArr[buttonsPressed] = 5;  // Store 5 input in buttons array
-      buttonsPressed++;
-      delay(200);
-      Serial.println(buttonsPressed);
+      continue;
+    } else if (buttonVal <= 180) {  // button 0 pressed
+      buttonsArr[buttonsPressed] = 0;
+    } else if (buttonVal <= 220) {  // button 1 pressed
+      buttonsArr[buttonsPressed] = 1;
+    } else if (buttonVal <= 290) {  // button 2 pressed
+      buttonsArr[buttonsPressed] = 2;
+    } else if (buttonVal <= 400) {  // button 3 pressed
+      buttonsArr[buttonsPressed] = 3;
+    } else if (buttonVal <= 750) {  // button 4 pressed
+      buttonsArr[buttonsPressed] = 4;
+    } else if (buttonVal <= 1024) {  // button 5 pressed
+      buttonsArr[buttonsPressed] = 5;
     }
+    buttonsPressed++;
+    delay(200);  // delay to avoid multiple inputs
+    Serial.print(buttonsPressed);
+    Serial.println();
   }
 
   Serial.print("Input was: ");
 
-  for (int i = 0; i < 4; i++) {  // Prints stored code in terminal
+  for (int i = 0; i < 4; i++) {
     Serial.print(buttonsArr[i]);
   }
 
