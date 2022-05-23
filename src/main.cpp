3b194697fbc893572c7a11e0715992187af424dd
Felix Chan <85690455+fc1018@users.noreply.github.com>
2022-05-23 12:15:15 +0100

    Remastered version

--- src/main.cpp
+++ src/main.cpp
@@ -2,28 +2,28 @@
 #include <Servo.h>
 
 Servo lock;             // Locking mechanism
-const int button = A0;  // Analog read of voltage at end of resistor ladder
+const int button = A0;  // Analog read of voltage at end of transistor ladder
+int passcode[4];
+int attempt[4];
 
-int passcode = 0;
-int attempt = 0;
-
-int readButtons(int& passcode);
+int readbuttons(int buttonsArr[4]);
 bool isPasscodeRight();
 
 void setup() {
   lock.attach(3);
   Serial.begin(9600);
 
-  lock.write(0);  // Lock deactivated
+  lock.write(0)
+      :  // Lock deactivated
 
-  readButtons(passcode);
+        readbuttons(passcode);
 
-  lock.write(90);  // Lock activated
+  lock.write(90) :  // Lock activated
 }
 
 void loop() {
   do {
-    readButtons(attempt);
+    readbuttons(attempt);
     isPasscodeRight();
   } while (isPasscodeRight() == false);
 
@@ -34,37 +34,56 @@ void loop() {
   lock.write(90);  // lock reactivated
 }
 
-int readButtons(int& passcode) {  // Function to take button inputs and
-                                  // store in input array
+int readButtons(int buttonsArr[4]) {  // Function to take button inputs and
+                                      // store in input array
+                                      // array
   int buttonsPressed = 0;
+
   while (buttonsPressed < 4) {
     int buttonVal = analogRead(A0);  // Read voltage at end of resistor ladder
 
     if (buttonVal < 50) {  // Ignore noise
       continue;
     } else if (buttonVal <= 180) {  // button 0 pressed
-      // passcode += 0 * round(pow(10, 3 - buttonsPressed));
+      buttonsArr[buttonsPressed] = 0;
     } else if (buttonVal <= 220) {  // button 1 pressed
-      passcode += 1 * round(pow(10, 3 - buttonsPressed));
+      buttonsArr[buttonsPressed] = 1;
     } else if (buttonVal <= 290) {  // button 2 pressed
-      passcode += 2 * round(pow(10, 3 - buttonsPressed));
+      buttonsArr[buttonsPressed] = 2;
     } else if (buttonVal <= 400) {  // button 3 pressed
-      passcode += 3 * round(pow(10, 3 - buttonsPressed));
+      buttonsArr[buttonsPressed] = 3;
     } else if (buttonVal <= 750) {  // button 4 pressed
-      passcode += 4 * round(pow(10, 3 - buttonsPressed));
+      buttonsArr[buttonsPressed] = 4;
     } else if (buttonVal <= 1024) {  // button 5 pressed
-      passcode += 5 * round(pow(10, 3 - buttonsPressed));
+      buttonsArr[buttonsPressed] = 5;
     }
     buttonsPressed++;
     delay(200);  // delay to avoid multiple inputs
+    Serial.print(buttonsPressed);
+    Serial.println();
   }
 
   Serial.print("Input was: ");
 
-  Serial.print(passcode);
+  for (int i = 0; i < 4; i++) {
+    Serial.print(buttonsArr[i]);
+  }
+
   Serial.println();
 
   return 0;
 }
 
-bool isPasscodeRight() { return passcode == attempt; }
+bool isPasscodeRight() {
+  if (passcode[0] != attempt[0]) {
+    return false;
+  } else if (passcode[1] != attempt[1]) {
+    return false;
+  } else if (passcode[2] != attempt[2]) {
+    return false;
+  } else if (passcode[3] != attempt[3]) {
+    return false;
+  } else {
+    return true;
+  }
+}
